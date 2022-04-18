a68731d72e9b14314b9c8d8af923bcb8ccbc27bf
yuki <kinome1989@outlook.jp>
2022-04-18 02:38:58 +0000

    Finish modeling-user

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220417032218) do
+ActiveRecord::Schema.define(version: 20220418022303) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
