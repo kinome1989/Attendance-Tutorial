2e577c72cbd6f3b00495fd5e2148522cd36dfba6
yuki <kinome1989@outlook.jp>
2022-05-02 03:18:54 +0000

    11.1.5終了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,8 @@
   
   # ログイン済みのユーザーか確認します。
   def logged_in_user
-    unless logged_in? store_location
+    unless logged_in? 
+      store_location
       flash[:danger] = "ログインしてください。"
       redirect_to login_url
     end
